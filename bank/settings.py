42413f7d5240d48a358d3432e25870366005eb11
Laíse Vieira <laisevdev@gmail.com>
2025-02-24 00:36:24 -0300

    add jwt settings

--- bank/settings.py
+++ bank/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'autenticacao',
+    'rest_framework_simplejwt',      
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',  
+    ),
+}
