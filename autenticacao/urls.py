fbf749abdf21b5a31f6efdc328c63d835805dc09
Laíse Vieira <laisevdev@gmail.com>
2025-02-25 17:21:16 -0300

    [feat]: add cadastrar url

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from autenticacao.views import protected_view, RegisterUserView
+from rest_framework_simplejwt.views import TokenObtainSlidingView, TokenRefreshSlidingView
+from autenticacao.views import RegisterUserView, register_user
 
 
 urlpatterns = [
-    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  
-     path('api/cadastro/', RegisterUserView.as_view(), name="register"),
+    path('api/login/', TokenObtainSlidingView.as_view(), name='token_obtain'),
+    path('api/token/refresh/', TokenRefreshSlidingView.as_view(), name='token_refresh'),  
+    path('api/cadastro/', RegisterUserView.as_view(), name="register"),
+    path('cadastrar/', register_user, name='register_user'),  # Nova rota para o templat
     #path('api/protected/', protected_view, name='protected_view'),
 ]
